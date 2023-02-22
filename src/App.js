9f398994c14fe681f1ae4e6ea48081c982f873df
MattWeinstein <102390205+MattWeinstein@users.noreply.github.com>
2023-02-21 21:32:24 -0500

    added error handling and comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import { FormatTypes } from '@ethersproject/abi';
 import { hexToDec } from 'hex2dec';
 import { Alchemy, Network, Utils } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
-
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
@@ -32,7 +22,6 @@ function App() {
     priceCurrency: ''
   })
 
-
   const blockChangeHandler = (event) => {
     setSelectedBlock(event.target.value)
   }
@@ -43,40 +32,48 @@ function App() {
     getNFTData(nftContract)
   }
 
+  // Fetch NFT and set it to state
   async function getNFTData(nftContract) {
-    const nft = await alchemy.nft.getFloorPrice(nftContract)
-    console.log('yoo', nft)
-    setNFT({
-      floorPrice: nft.openSea.floorPrice,
-      collectionUrl: nft.openSea.collectionUrl.split('https://opensea.io/collection/')[1],
-      priceCurrency: nft.openSea.priceCurrency
-    })
+    try {
+      const nft = await alchemy.nft.getFloorPrice(nftContract)
+
+      setNFT({
+        floorPrice: `Floor Price: ${nft.openSea.floorPrice}`,
+        collectionUrl: nft.openSea.collectionUrl.split('https://opensea.io/collection/')[1],
+        priceCurrency: nft.openSea.priceCurrency
+      })
+    } catch {
+      setNFT({
+        floorPrice: '',
+        collectionUrl: 'CONTRACT ADDRESS INCORRECT',
+        priceCurrency: ''
+      })
+    }
   }
 
+  // Fetch the current block number on page load
   useEffect(() => {
     async function getCurrentBlockNumber() {
       const currentBlockNumber = await alchemy.core.getBlockNumber();
       setBlockNumber(currentBlockNumber)
     }
     getCurrentBlockNumber();
-
   }, [])
 
+  // Update UI when different block is searched for
   useEffect(() => {
     async function getTransactions() {
       const blockData = await alchemy.core.getBlockWithTransactions(blockNumber - selectedBlock);
       setMinerWallet(blockData.miner)
       const minerWalletAmount = await alchemy.core.getBalance(blockData.miner)
       setMinerWalletAmountEther(Utils.formatEther((hexToDec(minerWalletAmount._hex)), "ether"))
-
       const transactionsArr = blockData.transactions.splice(0, 5)
-      console.log(transactionsArr)
       setTransactionsList(transactionsArr.map((element, index) => {
         return <li key={index}>{Utils.formatEther(hexToDec(element.value._hex))}ETH {element.hash.toString().slice(0, 8)}</li>
       }))
     }
     getTransactions()
-  }, [blockNumber]);
+  }, [blockNumber, selectedBlock]);
 
   return (
     <>
@@ -98,7 +95,7 @@ function App() {
         <input type='text'></input>
         <button >Enter</button>
       </form>
-      <div>{NFT.collectionUrl} Floor Price: {NFT.floorPrice} {NFT.priceCurrency}</div>
+      <div>{NFT.collectionUrl} {NFT.floorPrice} {NFT.priceCurrency}</div>
 
     </>
   )
